60b6c1038a9a385a3fd0519b66af31e4c81347ee
rayGreycloud <rayGreycloud@gmail.com>
2017-02-13 14:38:02 -0800

    Move css and js to separate files

---
+++ code_bin.js
@@ -0,0 +1,12 @@
+
+$(".toggleButton").hover(function() {
+  $(this).addClass("highlightedButton");
+}, function() {
+  $(this).removeClass("highlightedButton");
+});
+
+$(".toggleButton").click(function() {
+  $(this).toggleClass("active");
+
+  $(this).removeClass("highlightedButton");
+})
