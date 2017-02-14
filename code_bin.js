9d401c15021f58654bd053aad41a7f037b0178a1
rayGreycloud <rayGreycloud@gmail.com>
2017-02-14 12:59:03 -0800

    Add comments to code_bin.js

--- code_bin.js
+++ code_bin.js
@@ -1,10 +1,11 @@
-
+// Change button color on hover
 $(".toggleButton").hover(function() {
   $(this).addClass("highlightedButton");
 }, function() {
   $(this).removeClass("highlightedButton");
 });
 
+// Toggle fields on click
 $(".toggleButton").click(function() {
   $(this).toggleClass("active");
 
@@ -13,16 +14,18 @@ $(".toggleButton").click(function() {
   var panelId = $(this).attr("id") + "Panel";
 
   $("#" + panelId).toggleClass("hidden");
-
+  // Divide window among active panels
   var numberActivePanels = 4 - $(".hidden").length;
   $(".panel").width(($(window).width() / numberActivePanels) - 10);
 
 })
 
+// Set height of fields
 $(".panel").height($(window).height() - $("#header").height() - 15);
-
+// Set width for initial active fields
 $(".panel").width(($(window).width() / 2) - 10);
 
+// Change output field when other fields change 
 $("textarea").on('change keyup paste', function() {
   $("iframe").contents().find("html").html($("#htmlPanel").val());
 });
